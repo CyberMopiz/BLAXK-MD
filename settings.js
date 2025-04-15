cede64abdcba9cd2fc0b645e7ba5a46409af5ad0
CyberMopiz <fertilizer.mopiz@gmail.com>
2025-04-15 11:17:33 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'Ferris',
+  author: 'Ferris',
+  botName: "CyberFerrisMC[愛は死によるもの❤︎]",
+  botOwner: 'Ferris Mashels', // Your name
+  ownerNumber: '6289529546720', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "[I.T.F. Syndicates]•ただの怠け者°-°[YT: >CyberFerrisMC<]•copyrigth ©I.T.F. Syndicates 2020-2025",
   version: "1.0.0",
 };
 
